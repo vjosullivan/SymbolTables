9b4f06350e0bb8429f3d38dd398b72b1395fc874
vjosullivan <vjosullivan@gmail.com>
2016-12-06 22:01:14 +0000

    Initial Commit

---
+++ SymbolTables/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SymbolTables
+//
+//  Created by Vincent O'Sullivan on 06/12/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
