b3ca04f589f378648614d281a37c10ae4a24705b
vjosullivan <vjosullivan@gmail.com>
2016-12-10 11:09:34 +0000

    RBTree functions moved from test object.

--- SymbolTables/tables/RedBlackTree.swift
+++ SymbolTables/tables/RedBlackTree.swift
@@ -10,4 +10,66 @@ import Foundation
 
 class RedBlackTree: OrderedSymbolTable {
     
+    typealias Key = String
+    typealias Value = Int
+
+    func put(key: Key, value: Value?) {
+        // TODO:
+    }
+
+    func get(key: Key) -> Value? {
+        // TODO:
+        return nil
+    }
+
+    func size() -> Int {
+        // TODO:
+        return 0
+    }
+
+    func keys() -> [Key] {
+        // TODO:
+        return [Key]()
+    }
+
+    func min() -> Key? {
+        // TODO:
+        return nil
+    }
+
+    func max() -> Key? {
+        // TODO:
+        return nil
+    }
+
+    func floor(key: Key) -> Key? {
+        // TODO:
+        return nil
+    }
+
+
+    func ceiling(key: Key) -> Key? {
+        // TODO:
+        return nil
+    }
+
+    func rank(key: Key) -> Int {
+        // TODO:
+        return 0
+    }
+
+    func select(rank: Int) -> Key? {
+        // TODO:
+        return nil
+    }
+
+    func keys(fromKey lo: Key, toKey hi: Key) -> [Key] {
+        // TODO:
+        return [Key]()
+    }
+
+    func sortedKeys() -> [Key] {
+        // TODO:
+        return [Key]()
+    }
 }
