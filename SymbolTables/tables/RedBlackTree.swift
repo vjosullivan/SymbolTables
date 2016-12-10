87e158f84c7526f16ec1d84bb20e62aa5a74449f
Vince O'Sullivan <vjosullivan@gmail.com>
2016-12-10 09:35:47 +0000

    RB Tree added (and Appcode files).

---
+++ SymbolTables/tables/RedBlackTree.swift
@@ -0,0 +1,13 @@
+//
+//  RedBlackTree.swift
+//  SymbolTables
+//
+//  Created by Vincent O'Sullivan on 09/12/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import Foundation
+
+class RedBlackTree: OrderedSymbolTable {
+    
+}
---
+++ SymbolTablesTests/RedBlackTreeTests.swift
@@ -0,0 +1,73 @@
+//
+//  RedBlackTreeTests.swift
+//  SymbolTables
+//
+//  Created by Vincent O'Sullivan on 09/12/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import XCTest
+@testable import SymbolTables
+
+class RedBlackTreeTests: XCTestCase {
+
+    func put(key: Key, value: Value?) {
+        // TODO:
+    }
+    
+    func get(key: Key) -> Value? {
+        // TODO:
+        return nil
+    }
+        
+    func size() -> Int {
+        // TODO:
+        return 0
+    }
+    
+    func keys() -> [Key] {
+        // TODO:
+        return [Key]()
+    }
+
+    func min() -> Key? {
+        // TODO:
+        return nil
+    }
+
+    func max() -> Key? {
+        // TODO:
+        return nil
+    }
+    
+    func floor(key: Key) -> Key? {
+        // TODO:
+        return nil
+    }
+
+    
+    func ceiling(key: Key) -> Key? {
+        // TODO:
+        return nil
+    }
+    
+    func rank(key: Key) -> Int {
+        // TODO:
+        return 0
+    }
+    
+    func select(rank: Int) -> Key? {
+        // TODO:
+        return nil
+    }
+    
+    func keys(fromKey lo: Key, toKey hi: Key) -> [Key] {
+        // TODO:
+        return [Key]()
+    }
+    
+    func sortedKeys() -> [Key] {
+        // TODO:
+        return [Key]()
+    }
+}
